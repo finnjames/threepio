f8e049a957612eb36109306bfe1ba2316cdf920d
Finn <finn.s.james@gmail.com>
2019-09-27 23:45:26 -0400

    clock, also ui images

--- threepio.py
+++ threepio.py
@@ -1,6 +1,13 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
-from main_ui import Ui_MainWindow # pre compiled PyQt ui
+from main_ui import Ui_MainWindow  # pre compiled PyQt main ui
+from dialog_ui import Ui_Dialog    # pre compiled PyQt dialogue ui
 import time
+
+class Dialog(QtWidgets.QDialog): # new observation dialog
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
 
 class Threepio(QtWidgets.QMainWindow): # whole app class
     def __init__(self):
@@ -9,8 +16,14 @@ class Threepio(QtWidgets.QMainWindow): # whole app class
         self.ui.setupUi(self)
         self.ui.chart_clear_button.clicked.connect(self.handleHello)
 
+        # timer for clock
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.handleHello)
+        self.timer.start(1000)
+
     def handleHello(self):
-        self.ui.ra_value.setText(time.gmtime(time.clock_gettime(time.CLOCK_REALTIME)))
+        current_time = time.localtime(time.clock_gettime(time.CLOCK_REALTIME))
+        self.ui.ra_value.setText(str(current_time[3]) + " : " + str(current_time[4]) + " : " + str(current_time[5]))
 
 
 if __name__ == '__main__':
