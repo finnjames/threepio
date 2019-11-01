c9eeafdd525aac5317d1cbd152b7964a1d4dac34
Finn <finn.s.james@gmail.com>
2019-11-01 16:08:45 -0400

    observation types

--- threepio.py
+++ threepio.py
@@ -15,18 +15,27 @@ from PyQt5 import QtCore, QtWidgets, QtGui, QtChart
 from main_ui import Ui_MainWindow   # compiled PyQt main ui
 from dialog_ui import Ui_Dialog     # compiled PyQt dialogue ui
 from astropy.time import Time
+from playsound import playsound # TODO: test on Windows
 import numpy as np
 import time, math, random
 
 import tars
+import precious
 
 class Dialog(QtWidgets.QDialog):
     """New observation dialogue window"""
-    def __init__(self, parent_window, date):
+    def __init__(self, parent_window, date, observation):
         QtWidgets.QWidget.__init__(self)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.setWindowTitle("Threepio Dialogue")
+        self.observation = observation
+        
+        # If a scan, only one Dec needed
+        if self.observation.obs_type == "Scan":
+            self.ui.ending_dec.hide()
+            self.ui.end_dec_label.hide()
+            self.ui.start_dec_label.setText("Declination")
         
         # set today's date
         self.current_date = QtCore.QDate(time.localtime(date)[0], time.localtime(date)[1], time.localtime(date)[2])
@@ -36,27 +45,44 @@ class Dialog(QtWidgets.QDialog):
         self.ui.end_time.setDate(self.current_date)
         self.ui.end_time.setTime(self.current_time)
 
-        # store the window that spawned it
+        # store parent window
         self.parent_window = parent_window
 
-        # when "ok" pressed
+        # connect okay button
         self.ui.dialog_button_box.accepted.connect(self.handle_ok)
 
     def handle_ok(self):
+        self.set_observation()
+        self.parent_window.observation = None
+        self.parent_window.observation = self.observation
+        
+        # this should fix the stripchart, #nojudgement
+        self.parent_window.stripchart_offset = 0
+        self.parent_window.stripchart_series_a.clear()
+        self.parent_window.stripchart_series_b.clear()
+        
+    def set_observation(self):
         pattern = "%Y.%m.%d %H:%M:%S"
-        start = int(time.mktime(time.strptime(self.ui.start_time.text(), pattern)))
-        end = int(time.mktime(time.strptime(self.ui.end_time.text(), pattern)))
-
-        self.parent_window.set_ra(start, end)
+        
+        self.observation.set_RA(int(time.mktime(time.strptime(self.ui.start_time.text(), pattern))), int(time.mktime(time.strptime(self.ui.end_time.text(), pattern))))
+        self.observation.set_dec(self.ui.starting_dec.text(), self.ui.ending_dec.text())
+        
+        self.observation.set_precious(self.ui.file_name_value)
+        print(self.observation.precious.filename)
+        
 
 class SuperClock():
     """Clock object for encapsulation; keeps track of the time(tm)"""
-    def __init__(self, starting_time = time.time()):
-        self.starting_time = starting_time
+    
+    SIDEREAL = 1.00273790935
+    
+    def __init__(self, starting_sidereal_time = time.time()):
+        self.starting_time = time.time()
+        self.starting_sidereal_time = starting_sidereal_time
     
     def get_time(self):
         return time.time()
-
+    
     def get_elapsed_time(self):
         return time.time() - self.starting_time
     
@@ -67,15 +93,78 @@ class SuperClock():
         return time.localtime(time.time())
     
     def get_sidereal_time(self): # TODO: make this actually be sidereal
-        sidereal = Time(time.time(), format="unix")
+        sidereal = self.starting_sidereal_time + ((time.time() - self.starting_time) * self.SIDEREAL)
+        print(time.time(), sidereal)
+        sidereal = Time(sidereal, format="unix")
         return sidereal.fits[-12:][:11] # TODO: make this less wack
     
+class Observation():
+    """superclass for each of the three types of observation you might encounter on your Pokemon journey"""
+    
+    obs_type = None
+    
+    data = []
+    written_to = -1
+    
+    start_RA = 0
+    end_RA = 0
+    start_dec = 0 # if only one dec, this is it
+    end_dec = 0
+    
+    def __init__(self):
+        pass
+    
+    def set_RA(self, start_RA, end_RA):
+        self.start_RA = start_RA
+        self.end_RA = end_RA
+    
+    def set_dec(self, start_dec, end_dec):
+        self.start_dec = start_dec
+        self.end_dec = end_dec
+        
+    def set_precious(self, filename):
+        self.precious = precious.MyPrecious(filename)
+    
+    def add_data(self, data_point):
+        self.data.append(data_point)
+    
+    def get_last_data(self):
+        return self.data[len(self.data) - 1]
+    
+    # TODO: implement MyPrecious encapsulation
+
+
+class Scan(Observation):
+    """for all your scan observation needs"""
+    obs_type = "Scan"
+    data = []
+    
+    def set_dec(self, start_dec, end_dec):
+        self.start_dec = start_dec
+
+class Survey(Observation):
+    """for all your survey observation needs"""
+    obs_type = "Survey"
+    data = []
+
+class Spectrum(Observation):
+    """for all your spectrum observation needs"""
+    obs_type = "Spectrum"
+    data = []
+    
+    def set_dec(self, start_dec, end_dec):
+        self.start_dec = start_dec
+
 class DataPoint():
     """each data point taken, has a timestamp and two voltage channels"""
-    def __init__(self, timestamp, a, b):
+    def __init__(self, timestamp, a, b, dec):
         self.timestamp = timestamp
         self.a = a
         self.b = b
+        self.dec = dec
+        
+    def to_tuple(self):
+        return (self.timestamp, self.a, self.b, self.dec)
         
 class Threepio(QtWidgets.QMainWindow):
     """Main class for the app"""
@@ -90,11 +179,11 @@ class Threepio(QtWidgets.QMainWindow):
     # test data
     ticker = 0
     other_ticker = 0
-    
+
     # stripchart
     stripchart_low = -1
     stripchart_high = 1
-    
+
     # palette
     BLUE = 0x2196f3
     RED = 0xff5252
@@ -118,20 +207,19 @@ class Threepio(QtWidgets.QMainWindow):
         self.ui.actionSpectrum.triggered.connect(self.handle_spectrum)
 
         self.ui.chart_clear_button.clicked.connect(self.handle_clear)
-        self.ui.chart_refresh_button.clicked.connect(self.handle_refresh)
+        # self.ui.chart_refresh_button.clicked.connect(self.handle_refresh)
+        # this is for testing, the above line is the correct one
+        self.ui.chart_refresh_button.clicked.connect(self.handle_scan)
 
         # TODO: maybe just choose one of these?
         self.ui.actionLegacy.triggered.connect(self.legacy_mode)
         self.ui.chart_legacy_button.clicked.connect(self.legacy_mode)
 
-        # init data array
-        self.data = []
-
         # initialize stripchart
         self.stripchart_series_a = QtChart.QLineSeries()
         self.stripchart_series_b = QtChart.QLineSeries()
         self.ui.stripchart.setRenderHint(QtGui.QPainter.Antialiasing)
-        
+
         # make the charts m a t e r i a l (blue and red)
         pen = QtGui.QPen(QtGui.QColor(self.BLUE))
         pen.setWidth(3)
@@ -139,7 +227,7 @@ class Threepio(QtWidgets.QMainWindow):
         pen.setColor(QtGui.QColor(self.RED))
         self.stripchart_series_b.setPen(pen)
 
-        # DATAQ stuff
+        # # DATAQ stuff
         # self.tars = tars.Tars(tars.discovery())
         # self.tars.init()
         # self.tars.start()
@@ -148,7 +236,10 @@ class Threepio(QtWidgets.QMainWindow):
         self.clock = SuperClock(time.time())
         self.start_RA = time.time()
         self.end_RA = time.time()
-
+        
+        # blank obs
+        self.observation = Observation()
+        
         # refresh timer
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.tick) # do everything
@@ -158,12 +249,11 @@ class Threepio(QtWidgets.QMainWindow):
 
         self.update_gui() # update gui
 
-        # self.data.append(int(((math.sin((self.ticker / (8 * math.pi)))*300)**2))) # pretty sine wave
         self.ticker += random.random()*random.randint(-1,1)       # \ for the test data
         self.other_ticker += random.random()*random.randint(-1,1) # /
-        self.data.append(DataPoint(self.clock.get_time(), self.ticker, self.other_ticker)) # random meander
+        self.observation.add_data(DataPoint(self.clock.get_time(), self.ticker, self.other_ticker, 1)) # random meander
 
-        # self.data.append(self.tars.read_one(1)) # get data from DAQ
+        # self.observation.add_data(self.tars.read_one(1)) # get data from DAQ
         
         self.update_strip_chart() # make the stripchart scroll
         
@@ -182,53 +272,35 @@ class Threepio(QtWidgets.QMainWindow):
             self.stripchart_display_ticks = 2048
         self.handle_clear()
 
-    def set_ra(self, start_RA, end_RA):
-        self.start_RA = start_RA
-        self.end_RA = end_RA
-
     def update_gui(self):
         self.ui.ra_value.setText(self.clock.get_sidereal_time() + "UTC")
         
         # TODO: get data from declinometer
-        if len(self.data) > 0:
-            self.ui.channelA_value.setText("%.2f" % (self.data[len(self.data) - 1].a))
-            self.ui.channelB_value.setText("%.2f" % (self.data[len(self.data) - 1].b))
+        self.ui.dec_value.setText(str(self.observation.start_dec) + "deg")
+        
+        if len(self.observation.data) > 0:
+            self.ui.channelA_value.setText("%.2f" % (self.observation.data[len(self.observation.data) - 1].a))
+            self.ui.channelB_value.setText("%.2f" % (self.observation.data[len(self.observation.data) - 1].b))
         
         # this mess makes the progress bar display "T+/- XX.XX" when
         # assigned, and progress the bar when taking data
-        if not self.end_RA - self.start_RA <= 1:
-            if self.clock.get_time_until(self.start_RA) > 0 and self.clock.get_time_until(self.end_RA) < 0:
-                self.ui.progressBar.setValue(int((self.clock.get_time_until(self.end_RA) / (self.end_RA - self.start_RA)) * 100 % 100))
+        if not self.observation.end_RA - self.observation.start_RA <= 1:
+            if self.clock.get_time_until(self.observation.start_RA) > 0 and self.clock.get_time_until(self.observation.end_RA) < 0:
+                self.ui.progressBar.setValue(int((self.clock.get_time_until(self.observation.end_RA) / (self.observation.end_RA - self.observation.start_RA)) * 100 % 100))
             else:
                 self.ui.progressBar.setValue(0)
-            self.ui.progressBar.setFormat("T%+.1fs" % (self.clock.get_time_until(self.start_RA)))
+            self.ui.progressBar.setFormat("T%+.1fs" % (self.clock.get_time_until(self.observation.start_RA)))
         else:
             self.ui.progressBar.setFormat("n/a")
             self.ui.progressBar.setValue(0)
-
-    def handle_survey(self):
-        self.new_observation("survey")
-
-    def handle_scan(self):
-        self.new_observation("scan")
-
-    def handle_spectrum(self):
-        self.new_observation("spectrum")
-
-    def new_observation(self, type):
-        dialog = Dialog(self, self.clock.get_time())
-        dialog.setWindowTitle("New " + type.capitalize())
-        dialog.show()
-        dialog.exec_()
     
     def update_strip_chart(self):
-        new_a = self.data[len(self.data) - 1].a
-        new_b = self.data[len(self.data) - 1].b
-        self.stripchart_series_a.append(new_a, len(self.data))
-        self.stripchart_series_b.append(new_b, len(self.data))
+        new_a = self.observation.data[len(self.observation.data) - 1].a
+        new_b = self.observation.data[len(self.observation.data) - 1].b
+        self.stripchart_series_a.append(new_a, len(self.observation.data))
+        self.stripchart_series_b.append(new_b, len(self.observation.data))
 
-        while (len(self.data) - self.stripchart_offset > self.stripchart_display_ticks):
-            # TODO: make this not lag when there's a big delta in display_ticks
+        while (len(self.observation.data) - self.stripchart_offset > self.stripchart_display_ticks):
             self.stripchart_series_a.remove(0)
             self.stripchart_series_b.remove(0)
             self.stripchart_offset += 1
@@ -274,6 +346,24 @@ class Threepio(QtWidgets.QMainWindow):
     def handle_refresh(self):
         self.stripchart_low = 32767
         self.stripchart_high = -32768
+
+    def handle_survey(self):
+        obs = Survey()
+        self.new_observation(obs)
+
+    def handle_scan(self):
+        obs = Scan()
+        self.new_observation(obs)
+
+    def handle_spectrum(self):
+        obs = Spectrum()
+        self.new_observation(obs)
+
+    def new_observation(self, obs):
+        dialog = Dialog(self, self.clock.get_time(), obs)
+        dialog.setWindowTitle("New " + obs.obs_type)
+        dialog.show()
+        dialog.exec_()
 
 
 def main():
