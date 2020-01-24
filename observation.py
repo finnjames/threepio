2b9f57e769443eabe457b41f184773527658976b
shengjiex98 <shengjiex98@gmail.com>
2020-01-24 15:12:41 -0500

    observation logic 2

--- observation.py
+++ observation.py
@@ -3,20 +3,17 @@ import time
 from datapoint import DataPoint
 
 class Observation():
-    """superclass for each of the three types of observation you might encounter on your Pokemon journey"""
+    """Superclass for each of the three types of observation you might encounter on your Pokemon journey"""
     
-    obs_type = None
-    
-    def __init__(self, name, start_RA, end_RA, start_dec, end_dec):
+    def __init__(self, name, start_RA, end_RA, max_dec, min_dec):
         self.name       = name
-        self.data       = []
-        self.compsite   = False
+        self.composite   = False
         self.state      = "STOPPED"
         
         self.start_RA   = start_RA
         self.end_RA     = end_RA
-        self.start_dec  = start_dec     # if only one dec, this is it
-        self.end_dec    = end_dec
+        self.max_dec    = max_dec     # if only one dec, this is it
+        self.min_dec    = min_dec
 
         self.start_time = None
         self.end_time   = None
@@ -26,10 +23,11 @@ class Observation():
         # Temporary bookkeeping
         self.cal_start  = None
         self.bg_start   = None
-
-    def data_logic(self, data_point):
-        pass
+        
+        # The data list, for backup
+        self.data       = []
     
+    # This is the communication API
     def communicate(self, data_point):
         if self.state == "STOPPED":
             if time.time() < self.start_RA - 120:
@@ -68,23 +66,7 @@ class Observation():
         elif self.state == "FINISHED":
             return "Observation is already finished"
 
-    def set_RA(self, start_RA, end_RA):
-        self.start_RA = start_RA
-        self.end_RA = end_RA
-    
-    def set_dec(self, start_dec, end_dec):
-        self.start_dec = start_dec
-        self.end_dec = end_dec
-        
-    def set_files(self, extension):
-        self.file_a = self.name + '_a' + extension
-        self.file_b = self.name + '_b' + extension
-        self.file_comp = self.name + '_comp' + extension
-    
-    def get_last_data(self):
-        return self.data[len(self.data) - 1]
-    
-    # TODO: implement MyPrecious encapsulation
+    # These are the action APIs
     def start(self):
         self.state = "CAL ON 1"
         self.start_time = time.time()
@@ -92,61 +74,61 @@ class Observation():
 
     def end_calibration_1(self):
         self.state = "BG 1"
-        if self.compsite:
-            self.file_comp.write('*')
-        else:
-            self.file_a.write('*')
-            self.file_b.write('*')
+        self.write('*')
         self.bg_start = time.time()
         
     def end_background_1(self):
         self.state = "DATA"
-        if self.compsite:
-            self.file_comp.write('*')
-        else:
-            self.file_a.write('*')
-            self.file_b.write('*')
+        self.write('*')
         self.data_start = time.time()
 
     def end_data(self):
         self.state = "CAL ON 2"
-        if self.compsite:
-            self.file_comp.write('*')
-        else:
-            self.file_a.write('*')
-            self.file_b.write('*')
+        self.write('*')
         self.data_end = time.time()
         self.cal_start = time.time()
 
     def end_calibration_2(self):
         self.state = "BG 2"
-        if self.compsite:
-            self.file_comp.write('*')
-        else:
-            self.file_a.write('*')
-            self.file_b.write('*')
+        self.write('*')
         self.bg_start = time.time()
 
-    def end_background_2(self):
-        self.stop()
-        self.state = "Finished"
-        
     def stop(self):
-        self.state = "STOPPED"
+        self.state = "Finished"
         self.end_time = time.time()
-        if self.compsite:
-            self.file_comp.write('*')
-            self.file_comp.write('*')
-        else:
-            self.file_a.write('*')
-            self.file_a.write('*')
-            self.file_b.write('*')
-            self.file_b.write('*')
+        self.write('*')
+        self.write('*')
         self.write_meta()
 
+    ############################ Helpers ############################
+    def set_RA(self, start_RA, end_RA):
+        self.start_RA = start_RA
+        self.end_RA = end_RA
+    
+    def set_dec(self, max_dec, min_dec):
+        self.max_dec = max_dec
+        self.min_dec = min_dec
+        
+    def set_files(self, extension):
+        self.file_a = self.name + '_a' + extension
+        self.file_b = self.name + '_b' + extension
+        self.file_comp = self.name + '_comp' + extension
+
+    def get_last_data(self):
+        return self.data[len(self.data) - 1]
+
+    def data_logic(self, data_point):
+        pass
+
+    def write(self, string: str):
+        if self.composite:
+            self.file_comp.write(string)
+        else:
+            self.file_a.write(string)
+            self.file_b.write(string)
+
     def write_data(self, point: DataPoint):
-        self.data.append(point)
-        if self.compsite:
+        if self.composite:
             self.file_comp.write(point.timestamp)
             self.file_comp.write(point.dec)
             self.file_comp.write(point.a)
@@ -160,29 +142,17 @@ class Observation():
             self.file_b.write(point.b)
 
     def write_meta(self):
-        if self.compsite:
-            self.file_comp.write('TELESCOPE: The Mighty Forty')
-            self.file_comp.write('LOCAL START DATE: ' + str(self.start_time.date()))
-            self.file_comp.write('LOCAL START TIME: ' + str(self.start_time.time()))
-            self.file_comp.write('LOCAL STOP DATE: '  + str(self.end_time.date()))
-            self.file_comp.write('LOCAL STOP TIME: '  + str(self.end_time.time()))
-        else:
-            self.file_a.write('TELESCOPE: The Mighty Forty')
-            self.file_a.write('LOCAL START DATE: ' + str(self.start_time.date()))
-            self.file_a.write('LOCAL START TIME: ' + str(self.start_time.time()))
-            self.file_a.write('LOCAL STOP DATE: '  + str(self.end_time.date()))
-            self.file_a.write('LOCAL STOP TIME: '  + str(self.end_time.time()))
-            self.file_b.write('TELESCOPE: The Mighty Forty')
-            self.file_b.write('LOCAL START DATE: ' + str(self.start_time.date()))
-            self.file_b.write('LOCAL START TIME: ' + str(self.start_time.time()))
-            self.file_b.write('LOCAL STOP DATE: '  + str(self.end_time.date()))
-            self.file_b.write('LOCAL STOP TIME: '  + str(self.end_time.time()))
+        self.write('TELESCOPE: The Mighty Forty')
+        self.write('LOCAL START DATE: ' + str(self.start_time.date()))
+        self.write('LOCAL START TIME: ' + str(self.start_time.time()))
+        self.write('LOCAL STOP DATE: '  + str(self.end_time.date()))
+        self.write('LOCAL STOP TIME: '  + str(self.end_time.time()))
 
 class Scan(Observation):
     """Set a start and end RA"""
 
-    def __init__(self, name, start_RA, end_RA, start_dec, end_dec):
-        super().__init__(name, start_RA, end_RA, start_dec, end_dec)
+    def __init__(self, name, start_RA, end_RA, max_dec, min_dec):
+        super().__init__(name, start_RA, end_RA, max_dec, min_dec)
         self.set_files('.md1')
 
     def data_logic(self, data_point):
@@ -192,18 +162,29 @@ class Scan(Observation):
 class Survey(Observation):
     """Set a region in sky using start and end RA/DEC"""
 
-    def __init__(self, name, start_RA, end_RA, start_dec, end_dec):
-        super().__init__(name, start_RA, end_RA, start_dec, end_dec)
+    def __init__(self, name, start_RA, end_RA, max_dec, min_dec):
+        super().__init__(name, start_RA, end_RA, max_dec, min_dec)
         self.set_files('.md2')
+        self.out_boundary = False
         
     def data_logic(self, data_point):
-        pass
+        if data_point.dec < self.min_dec or data_point.dec > self.max_dec:
+            if self.out_boundary:
+                return "Beep!"
+            else:
+                self.write('*')
+                self.out_boundary = True
+                return "Beep!"
+        else:
+            self.out_boundary = False
+            self.write_data(data_point)
+            return 0
 
 class Spectrum(Observation):
     """for all your spectrum observation needs"""
 
-    def __init__(self, name, start_RA, end_RA, start_dec, end_dec):
-        super().__init__(name, start_RA, end_RA, start_dec, end_dec)
+    def __init__(self, name, start_RA, end_RA, max_dec, min_dec):
+        super().__init__(name, start_RA, end_RA, max_dec, min_dec)
         self.set_files('.md2')
         
     def data_logic(self, data_point):
