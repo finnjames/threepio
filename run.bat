9a7daa2d7e7de602486b4008b8ea947a898eb6f7
Chewie <finn.s.james@gmail.com>
2020-06-10 20:32:53 -0700

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cmd /k "cd C:\Users\Chewie\Documents\GitHub\verdant-shores\threepio\env\Scripts & activate & cd /d C:\Users\Chewie\Documents\GitHub\verdant-shores\threepio & python threepio.py
+PAUSE
