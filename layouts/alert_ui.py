81d684ddd0bd112aae56f2b70642e3de83875c8f
Finn <finn@fsj.xyz>
2022-03-15 18:47:06 -0400

    alerts should be modal

--- layouts/alert_ui.py
+++ layouts/alert_ui.py
@@ -15,6 +15,7 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(320, 100)
+        Dialog.setModal(True)
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName("gridLayout")
         self.alert = QtWidgets.QLabel(Dialog)
